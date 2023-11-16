d07597199f8aa29b6f11a60529701962a2ffa1b5
tuanhq <tuanhq@vnext.vn>
2023-11-16 10:15:15 +0700

    Intercepter API

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getAllAccount } from "../../datasource/Account";
 
 const HomePage = () => {
+  const handleTestAPI = async () => {
+    try {
+      const rs = await getAllAccount();
+      console.log("🚀 - handleTestAPI - rs: ", rs);
+    } catch (error) {
+      console.log("🚀 - handleTestAPI - error: ", error);
+    }
+  };
+
+  useEffect(() => {
+    handleTestAPI();
+  }, []);
+
   return (
     <div>
       HomePage
